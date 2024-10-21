464148b5b3b6ea9cdab32b6707400b6ae4997eac
ExplorerGT92 <John.S@eloquent-algorithmics.com>
2024-10-20 20:54:20 -0400

    refactor: enhance sensor management and data collection in flask_air_fuel_datastream.py

--- src/datastreams/flask_air_fuel_datastream.py
+++ src/datastreams/flask_air_fuel_datastream.py
@@ -1,40 +1,90 @@
 import time
+import threading
+import re
 from flask import Flask, render_template_string, jsonify
 import obd
 from src.config import SERIAL_PORT, BAUD_RATE
 
-
 app = Flask(__name__)
 
-# Initialize lists for storing data
-timestamps = []
+# Initialize data structures
 SENSOR_DATA = {}
-supported_sensors = []
+timestamps = []
+sensors_info = []
 start_time = time.time()
 
+# Establish OBD-II connection
 connection = obd.OBD(portstr=SERIAL_PORT, baudrate=BAUD_RATE, fast=False)
+
+# Lock for thread-safe operations
+data_lock = threading.Lock()
+
+
+def sanitize_sensor_desc(desc):
+    """Sanitize sensor descriptions to create valid HTML IDs."""
+    return re.sub(r"\W|^(?=\d)", "_", desc)
 
 
 def check_and_add_sensor(sensor):
     """
     Checks if the given sensor exists in the database and adds it to the supported sensors list if it does.
-
-    Args:
-        sensor (str): The name or identifier of the sensor to be checked and added.
-
     Returns:
-        bool: True if the sensor was successfully added, False if the sensor does not exist in the database.
+        dict: sensor_info dict if successful, None otherwise.
     """
-    if connection.query(sensor).is_null():
-        return False
-    supported_sensors.append(sensor)
-    SENSOR_DATA[sensor] = []
-    return True
+    response = connection.query(sensor)
+    if response.is_null():
+        return None
+    sensor_desc = sensor.desc
+    sensor_id = sanitize_sensor_desc(sensor_desc)
+    SENSOR_DATA[sensor_id] = []
+    sensor_info = {"sensor": sensor, "desc": sensor_desc, "id": sensor_id}
+    return sensor_info
+
+
+def start_datastream():
+    global sensors_info
+    sensor_list = [
+        obd.commands["RPM"],
+        obd.commands["MAF"],
+        obd.commands["SHORT_FUEL_TRIM_1"],
+        obd.commands["SHORT_FUEL_TRIM_2"],
+        obd.commands["O2_B1S1"],
+        obd.commands["O2_B2S1"],
+        # Add other sensors as needed
+    ]
+
+    for sensor in sensor_list:
+        sensor_info = check_and_add_sensor(sensor)
+        if sensor_info:
+            sensors_info.append(sensor_info)
+
+
+def data_collector():
+    """Background thread for collecting sensor data."""
+    while True:
+        with data_lock:
+            for sensor_info in sensors_info:
+                sensor = sensor_info["sensor"]
+                sensor_id = sensor_info["id"]
+                response = connection.query(sensor)
+                sensor_data = None
+                if not response.is_null():
+                    value = response.value
+                    if value is not None:
+                        sensor_data = value.magnitude
+                SENSOR_DATA[sensor_id].append(sensor_data)
+                # Limit data length to last 500 points
+                if len(SENSOR_DATA[sensor_id]) > 500:
+                    SENSOR_DATA[sensor_id] = SENSOR_DATA[sensor_id][-500:]
+            # Append the timestamp
+            timestamps.append(time.time())
+            if len(timestamps) > 500:
+                timestamps[:] = timestamps[-500:]
+        time.sleep(0.2)  # Adjust the sleep time as needed
 
 
 @app.route("/")
 def index():
-    supported_sensors_desc = [sensor.desc for sensor in supported_sensors]
     return render_template_string(
         """
         <html>
@@ -58,29 +108,27 @@ def index():
                 </style>
             </head>
             <body>
-                {% for sensor_desc in supported_sensors %}
-                <div class="graph-container" id="{{ sensor_desc }}"></div>
+                {% for sensor in sensors_info %}
+                <div class="graph-container" id="{{ sensor.id }}"></div>
                 {% endfor %}
                 <script>
                     var graphs = {};
-
-                    {% for sensor_desc in supported_sensors %}
+                    {% for sensor in sensors_info %}
                     var trace = {
                         x: [],
                         y: [],
                         mode: 'lines',
-                        name: '{{ sensor_desc }}'
+                        name: '{{ sensor.desc }}'
                     };
-
                     var layout = {
-                        title: '{{ sensor_desc }}',
+                        title: '{{ sensor.desc }}',
                         xaxis: {
                             title: 'Elapsed Time (s)'
                         },
                         yaxis: {
                             title: 'Value'
                         },
-                        height: (100 / {{ num_sensors }}) + '%',
+                        height: '50%',
                         margin: {
                             t: 50,
                             l: 50,
@@ -101,107 +149,76 @@ def index():
                             }
                         }]
                     };
-
                     var config = {responsive: true};
-
-                    Plotly.newPlot('{{ sensor_desc }}', [trace], layout, config);
-                    graphs['{{ sensor_desc }}'] = trace;
+                    Plotly.newPlot('{{ sensor.id }}', [trace], layout, config);
+                    graphs['{{ sensor.id }}'] = {
+                        trace: trace,
+                        layout: layout
+                    };
                     {% endfor %}
-
                     function updateData() {
                         fetch('/data')
                             .then(response => response.json())
                             .then(data => {
-                                {% for sensor_desc in supported_sensors %}
-                                var sensor_desc = '{{ sensor_desc }}';
-                                var i = {{ loop.index0 }};
-                                graphs[sensor_desc].x = data.timestamps.map(ts => (ts - data.start_time));
-                                graphs[sensor_desc].y = data.sensor_data[i];
-                                var xAxisRange = [
-                                    graphs[sensor_desc].x[graphs[sensor_desc].x.length - 1] - 10,
-                                    graphs[sensor_desc].x[graphs[sensor_desc].x.length - 1]
-                                ];
-                                var updateRange = {'xaxis.range': xAxisRange};
-                                Plotly.relayout('{{ sensor_desc }}', updateRange);
-                                Plotly.update('{{ sensor_desc }}', {x: [graphs[sensor_desc].x], y: [graphs[sensor_desc].y]});
-                                var currentValue = data.sensor_data[i][data.sensor_data[i].length - 1];
-                                var labelText = 'Current value: ' + currentValue;
-                                if (sensor_desc === 'Engine RPM') {
-                                    labelText = 'Current RPM: ' + currentValue;
-                                } else if (sensor_desc === 'Mass Air Flow') {
-                                    labelText = 'Current gm/s: ' + currentValue;
+                                var timestamps = data.timestamps.map(ts => ts - data.start_time);
+                                for (var sensor_id in data.sensor_data) {
+                                    if (graphs.hasOwnProperty(sensor_id)) {
+                                        var graph = graphs[sensor_id];
+                                        graph.trace.x = timestamps;
+                                        graph.trace.y = data.sensor_data[sensor_id];
+                                        var xAxisRange = [
+                                            timestamps[timestamps.length -1] - 10,
+                                            timestamps[timestamps.length -1]
+                                        ];
+                                        var updateRange = {'xaxis.range': xAxisRange};
+                                        Plotly.relayout(sensor_id, updateRange);
+                                        Plotly.update(sensor_id, {x: [graph.trace.x], y: [graph.trace.y]});
+                                        var currentValue = data.sensor_data[sensor_id][data.sensor_data[sensor_id].length -1];
+                                        var labelText = 'Current value: ' + currentValue;
+                                        // Custom annotations based on sensor IDs
+                                        if (sensor_id === 'Engine_RPM') {
+                                            labelText = 'Current RPM: ' + currentValue;
+                                        } else if (sensor_id === 'Mass_Air_Flow') {
+                                            labelText = 'Current gm/s: ' + currentValue;
+                                        }
+                                        var update = {annotations: [{text: labelText}]};
+                                        Plotly.relayout(sensor_id, update);
+                                    }
                                 }
-                                var update = {annotations: [{text: labelText}]};
-                                Plotly.relayout('{{ sensor_desc }}', update);
-                                {% endfor %}
                             })
                             .catch(error => console.error(error));
-
                         setTimeout(updateData, 200);
                     }
-
                     updateData();
                 </script>
             </body>
         </html>
-    """,
-        num_sensors=len(supported_sensors),
-        supported_sensors=supported_sensors_desc,
+        """,
+        sensors_info=sensors_info,
+        start_time=start_time,
     )
 
 
 @app.route("/data")
 def data():
-    global timestamps, SENSOR_DATA
-
-    # Read the required OBD-II parameters
-    for sensor in supported_sensors:
-        response = connection.query(sensor)
-        SENSOR_DATA[sensor].append(response.value.magnitude)
-
-    # Append the timestamp
-    timestamps.append(time.time())
-
-    sensor_data_list = [SENSOR_DATA[sensor] for sensor in supported_sensors]
-
-    return jsonify(
-        {
-            "timestamps": timestamps,
-            "sensor_data": sensor_data_list,
-            "start_time": start_time,
+    with data_lock:
+        # Prepare sensor data for JSON
+        sensor_data_json = {
+            sensor_info["id"]: SENSOR_DATA[sensor_info["id"]]
+            for sensor_info in sensors_info
         }
-    )
-
-
-def start_datastream():
-
-    check_and_add_sensor(obd.commands.RPM)
-    check_and_add_sensor(obd.commands.MAF)
-    check_and_add_sensor(obd.commands.SHORT_FUEL_TRIM_1)
-    check_and_add_sensor(obd.commands.LONG_FUEL_TRIM_1)
-    check_and_add_sensor(obd.commands.SHORT_FUEL_TRIM_2)
-    check_and_add_sensor(obd.commands.LONG_FUEL_TRIM_2)
-    check_and_add_sensor(obd.commands.O2_B1S1)
-    check_and_add_sensor(obd.commands.O2_B2S1)
-    check_and_add_sensor(obd.commands.INTAKE_PRESSURE)
-    check_and_add_sensor(obd.commands.THROTTLE_POS)
-    check_and_add_sensor(obd.commands.O2_S1_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S2_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S2_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S3_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S3_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S4_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S4_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S5_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S5_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S6_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S6_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S7_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S7_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S7_WR_CURRENT)
-    check_and_add_sensor(obd.commands.O2_S8_WR_VOLTAGE)
-    check_and_add_sensor(obd.commands.O2_S8_WR_CURRENT)
+        return jsonify(
+            {
+                "timestamps": [t - start_time for t in timestamps],
+                "sensor_data": sensor_data_json,
+                "start_time": 0,  # Timestamps are already adjusted
+            }
+        )
 
 
 if __name__ == "__main__":
+    start_datastream()
+    # Start the data collection thread
+    data_thread = threading.Thread(target=data_collector, daemon=True)
+    data_thread.start()
     app.run(debug=False)
