d078e92d2307f7b1110466aed33e9f09767941e7
John Scott <John.S@eloquent-algorithmics.com>
2025-04-01 20:44:35 -0400

    chore: update installation scripts to include application startup instructions and fix conda environment name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create -n auto-ai python=3.12 -y
+conda create -n auto_ai python=3.12 -y
 
 echo "Installing Python requirements..."
 pip install -r requirements-dev.txt
@@ -9,3 +9,9 @@ echo "Downloading SpaCy model..."
 python -m spacy download en_core_web_md
 
 echo "Installation completed."
+
+echo "To continue, please activate the conda environment with the following command:"
+echo "conda activate auto_ai"
+
+echo "Then, run the following command to start the application:"
+echo "python automotive_ai/app.py"
